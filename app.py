fd170dea1a0423b5d9cff6089037c21ada6fb737
ABC <paridhishukla153@gmail.com>
2026-05-15 22:00:05 +0530

    Completed Day 2 component database

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+
+    with open("components.json", "r") as file:
+        components = json.load(file)
+
+    return render_template("index.html", components=components)
 
 if __name__ == "__main__":
     app.run(debug=True)
